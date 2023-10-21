20ddd01abcc7b758fd6921232e101701497dee5c
KhadijaGadirova <khadijagadirova1@gmail.com>
2023-10-21 23:56:09 +0400

    additional algorithm problems

--- src/algorithmProblems/AreAnagrams.java
+++ src/algorithmProblems/AreAnagrams.java
@@ -1,7 +1,27 @@
 package algorithmProblems;
 
+import java.util.Arrays;
+
 public class AreAnagrams {
-    public boolean areAnagrams(String s1, String s2) {
+    public static boolean areAnagrams(String s1, String s2) {
+
+
+        String trimmedS1 = s1.replace("//s", "").toLowerCase();
+        String trimmedS2 = s2.replace("//s", "").toLowerCase();
+
+        if (trimmedS1.length() != trimmedS2.length()) {
+            return false;
+        }
+        char[] charS1 = trimmedS1.toCharArray();
+        char[] charS2 = trimmedS2.toCharArray();
+
+        Arrays.sort(charS1);
+        Arrays.sort(charS2);
+        return Arrays.equals(charS1, charS2);
+
+    }
+
+    public boolean areAnagrams2(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
         }
@@ -16,6 +36,10 @@ public class AreAnagrams {
             charCount[c - 'a']--;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areAnagrams("khady", "hadyk"));
     }
 
 }
--- src/algorithmProblems/ReverseString.java
+++ src/algorithmProblems/ReverseString.java
@@ -1,12 +1,39 @@
 package algorithmProblems;
 
 public class ReverseString {
-    public String reverseString(String s) {
+    public static StringBuilder reverseString(String s) {
         StringBuilder reversed = new StringBuilder();
         for (int i = s.length() - 1; i >= 0; i--) {
             reversed.append(s.charAt(i));
         }
+        return reversed;
+    }
+
+    public static String reverseString2(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        StringBuilder reversed = new StringBuilder(str);
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            char temp = reversed.charAt(left);
+            reversed.setCharAt(left, reversed.charAt(right));
+            reversed.setCharAt(right, temp);
+
+            left++;
+            right--;
+        }
+
         return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        reverseString("khadija");
     }
 
 }
