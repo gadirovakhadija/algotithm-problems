6eef9076ba6a1fec321f650922d4ba7165072a89
KhadijaGadirova <khadijagadirova1@gmail.com>
2023-10-30 14:56:39 +0400

    deleteDublicate threeSum added

---
+++ src/algorithmProblems/DeleteDublicates.java
@@ -0,0 +1,37 @@
+package algorithmProblems;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class DeleteDublicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode numberList = head;
+        while (numberList.next != null) {
+            if (numberList.val == numberList.next.val)
+                numberList.next = numberList.next.next;
+            else
+                numberList = numberList.next;
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
