ec6208b575425d4018904227ac97134cf55ef6db
KhadijaGadirova <khadijagadirova1@gmail.com>
2024-02-01 00:08:05 +0400

    added palindromeString

---
+++ src/algorithmProblems/CodingChallenge/PalindromeString.java
@@ -0,0 +1,27 @@
+package algorithmProblems.CodingChallenge;
+
+public class PalindromeString {
+
+    public static boolean palindromeLoop(String s){
+        String reversed = "";
+        for (int i=s.length()-1;i>=0;i--){
+            reversed+=s.charAt(i);
+        }
+        if (s.equals(reversed)){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static boolean palindromeReversed(String s){
+        if (s.equals(ReverseString.reverseStringLoop(s))){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindromeLoop("salas"));
+    }
+}
