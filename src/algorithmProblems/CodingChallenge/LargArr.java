15d01bd2a039c57fa4dce369c8048caa26f9e7c6
KhadijaGadirova <khadijagadirova1@gmail.com>
2024-02-01 20:02:09 +0400

    added largestArr

---
+++ src/algorithmProblems/CodingChallenge/LargArr.java
@@ -0,0 +1,30 @@
+package algorithmProblems.CodingChallenge;
+
+import java.util.Arrays;
+
+public class LargArr {
+
+    public static int largArr(int[] arr){
+
+        int largest=arr[0];
+        for (int i=1;i<arr.length;i++){
+            if (arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        return largest;
+    }
+
+    public static int largArrSort(int[] arr){
+        Arrays.sort(arr);
+
+        return arr[arr.length-1];
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(largArrSort(new int[]{2,5,2,-4,3,3}));
+    }
+
+}
