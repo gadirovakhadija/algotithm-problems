28754f0f3398b02c6ff7fc245dce8526590dd4f9
KhadijaGadirova <khadijagadirova1@gmail.com>
2024-02-01 20:55:52 +0400

    averageArr added

---
+++ src/algorithmProblems/CodingChallenge/AverageArr.java
@@ -0,0 +1,23 @@
+package algorithmProblems.CodingChallenge;
+
+public class AverageArr {
+    public static double averageArrFor(int[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
+    public static double averageArrForEach(int[] arr){
+        double sum=0;
+        for (int elem:arr){
+            sum+=elem;
+        }
+        return sum/ arr.length;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(averageArrForEach(new int[]{2, 3, 11, 5, 5}));
+    }
+}
