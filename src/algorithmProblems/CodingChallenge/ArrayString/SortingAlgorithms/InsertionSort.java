41c87d075847c9744e4c3e86fa183599fc6918e9
KhadijaGadirova <khadijagadirova1@gmail.com>
2024-02-05 16:09:24 +0400

    insertionSort added

---
+++ src/algorithmProblems/CodingChallenge/ArrayString/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,36 @@
+package algorithmProblems.CodingChallenge.ArrayString.SortingAlgorithms;
+
+public class InsertionSort {
+
+    public static int[] insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: ");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("\nSorted Array: ");
+        printArray(array);
+    }
+}
