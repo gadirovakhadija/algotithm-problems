c4380f523c20790c9b621b3d6842f990085b104d
KhadijaGadirova <khadijagadirova1@gmail.com>
2024-02-06 20:14:14 +0400

    selectionSort added

---
+++ src/algorithmProblems/CodingChallenge/ArrayString/SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,41 @@
+package algorithmProblems.CodingChallenge.ArrayString.SortingAlgorithms;
+
+public class SelectionSort {
+
+    public static void selecSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: ");
+        printArray(array);
+
+        selecSort(array);
+
+        System.out.println("\nSorted Array: ");
+        printArray(array);
+    }
+
+}
