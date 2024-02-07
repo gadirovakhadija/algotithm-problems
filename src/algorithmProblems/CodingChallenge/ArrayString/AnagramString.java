871e51ab41be30cb3070b84026c2ceb84ec00308
KhadijaGadirova <khadijagadirova1@gmail.com>
2024-02-07 21:32:59 +0400

    anagramString added

---
+++ src/algorithmProblems/CodingChallenge/ArrayString/AnagramString.java
@@ -0,0 +1,24 @@
+package algorithmProblems.CodingChallenge.ArrayString;
+
+import java.util.Arrays;
+
+public class AnagramString {
+
+    public static boolean anagramString(String string1, String string2){
+
+        char[] c1 = string1.trim().toLowerCase().toCharArray();
+        char[] c2 = string2.trim().toLowerCase().toCharArray();
+
+        if (c1.length!=c2.length){
+            return false;
+        }
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        return Arrays.equals(c1,c2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(anagramString("xadica","Xdacia"));
+    }
+}
---
+++ src/algorithmProblems/CodingChallenge/ArrayString/RotateArrStep.java
@@ -0,0 +1,18 @@
+package algorithmProblems.CodingChallenge.ArrayString;
+
+public class RotateArrStep {
+
+//    public static int[] rotateArr(int[] arr, int k) {
+////2,3,4,5,6    4,5,6,2,3
+//        int num;
+//        if (arr.length == k) {
+//            return arr;
+//        } else {
+//
+//
+//
+//        }
+//
+//    }
+
+}
