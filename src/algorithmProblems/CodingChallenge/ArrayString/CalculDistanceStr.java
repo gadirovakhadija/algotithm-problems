967b69d851bb7d7d21a3ce9924ee03ac16b23bd8
KhadijaGadirova <khadijagadirova1@gmail.com>
2024-02-15 19:07:27 +0400

    rotateArrStep added

--- src/algorithmProblems/CodingChallenge/ArrayString/CalculDistanceStr.java
+++ src/algorithmProblems/CodingChallenge/ArrayString/CalculDistanceStr.java
@@ -4,24 +4,15 @@ public class CalculDistanceStr {
         int m = word1.length();
         int n = word2.length();
 
-        // Create a table to store results of subproblems
         int[][] dp = new int[m + 1][n + 1];
-
-        // Fill dp[][] in bottom-up manner
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
-                // If first string is empty, only option is to insert all characters of second string
                 if (i == 0)
-                    dp[i][j] = j; // Min. operations = j
-
-                    // If second string is empty, only option is to remove all characters of first string
+                    dp[i][j] = j;
                 else if (j == 0)
-                    dp[i][j] = i; // Min. operations = i
-
-                    // If last characters are same, ignore last character and recur for remaining strings
+                    dp[i][j] = i;
                 else if (word1.charAt(i - 1) == word2.charAt(j - 1))
                     dp[i][j] = dp[i - 1][j - 1];
-
                     // If last character are different, consider all possibilities and find minimum
                 else
                     dp[i][j] = 1 + Math.min(Math.min(dp[i][j - 1], // Insert
@@ -29,7 +20,6 @@ public class CalculDistanceStr {
                             dp[i - 1][j - 1]); // Replace
             }
         }
-
         return dp[m][n];
     }
 
--- src/algorithmProblems/CodingChallenge/ArrayString/RotateArrStep.java
+++ src/algorithmProblems/CodingChallenge/ArrayString/RotateArrStep.java
@@ -2,17 +2,37 @@ package algorithmProblems.CodingChallenge.ArrayString;
 
 public class RotateArrStep {
 
-//    public static int[] rotateArr(int[] arr, int k) {
-////2,3,4,5,6    4,5,6,2,3
-//        int num;
-//        if (arr.length == k) {
-//            return arr;
-//        } else {
-//
-//
-//
-//        }
-//
-//    }
+    public static void rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // in case k is greater than array length
+
+        reverse(nums, 0, n - 1);
+
+        reverse(nums, 0, k - 1);
+
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotateArray(nums, k);
+
+        // Print the rotated array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
 
 }
